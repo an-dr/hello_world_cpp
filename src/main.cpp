43cda2738f48fde908b39ff95b7cf879bd36a9c7
Andrei Gramakov <mail@agramakov.me>
2024-05-18 02:42:30 +0200

    Impoved formatting

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,29 @@
 // Copyright (c) 2024 Andrei Gramakov. All rights reserved.
 
+#include "main.hpp"
+#include "project_include.h"
+#include "project_include.hpp"
+#include <iostream>
 #include <stdio.h>
+#include <system_include.hpp>
+#include <vector>
 
-int main(int argc, char *argv[])
-{
-    if (argc > 1) {
-        printf("Hello, %s!\n", argv[1]);
-    } else {
-    printf("Hello!\n");
-    }
-    return 0;
+class Hello {
+ public:
+  void sayHelloWorld() { sayHello("World"); }
+
+  void sayHelloSomething(const char *smth) { sayHello(smth); }
+
+ private:
+  void sayHello(const char *smth) { printf("Hello, %s!\n", smth); }
+};
+
+int main(int argc, char *argv[]) {
+  Hello talker;
+  if (argc > 1) {
+    talker.sayHelloSomething(argv[1]);
+  } else {
+    talker.sayHelloWorld();
+  }
+  return 0;
 }
